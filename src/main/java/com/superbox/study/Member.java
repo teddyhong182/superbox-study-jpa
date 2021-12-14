309fa14bd5497679728fb88fad92dbdcdb15c841
Kiwon Hong <superbox@dtime.co>
2021-12-14 16:15:11 +0900

    기본 키 매핑 - SequenceGenerator 사용

--- src/main/java/com/superbox/study/Member.java
+++ src/main/java/com/superbox/study/Member.java
@@ -7,10 +7,16 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ",
+        initialValue = 1, allocationSize = 1
+)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     @Column(name = "name", nullable = false)
