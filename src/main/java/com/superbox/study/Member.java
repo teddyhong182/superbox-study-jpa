50df69425ea27dcfda59c256101d529fac4a3a9d
Kiwon Hong <superbox@dtime.co>
2021-12-14 15:52:35 +0900

    필드와 컬럼 매핑 - 매핑 어노테이션 정리

--- src/main/java/com/superbox/study/Member.java
+++ src/main/java/com/superbox/study/Member.java
@@ -1,36 +1,34 @@
 package com.superbox.study;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
+
+    @Column(name = "name", nullable = false)
     private String name;
 
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    private LocalDate createdDate;
+    private LocalDateTime lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
+
     public Member() {
-    }
-
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 }
---
+++ src/main/java/com/superbox/study/RoleType.java
@@ -0,0 +1,5 @@
+package com.superbox.study;
+
+public enum RoleType {
+    USER, ADMIN
+}
