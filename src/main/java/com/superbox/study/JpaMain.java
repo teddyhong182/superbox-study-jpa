c5ad9c8927778130d13ed1fa681e3e78593b0a20
Kiwon Hong <superbox@dtime.co>
2021-12-14 16:10:01 +0900

    기본 키 매핑 - persist 시점에 insert 넘어가는 경우

--- src/main/java/com/superbox/study/JpaMain.java
+++ src/main/java/com/superbox/study/JpaMain.java
@@ -15,10 +15,13 @@ public class JpaMain {
         tx.begin();
         try {
             Member member = new Member();
-
             member.setName("MemberJPA100");
-
+            // GenerationType.IDENTITY 를 사용하는 경우 persist 하는 시점에 insert query를 날림
+            System.out.println("=================");
             em.persist(member);
+            System.out.println("member.id = " + member.getId());
+            System.out.println("=================");
+
 
             System.out.println("=======");
             tx.commit();
--- src/main/java/com/superbox/study/Member.java
+++ src/main/java/com/superbox/study/Member.java
@@ -10,7 +10,7 @@ import java.util.Date;
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", nullable = false)
