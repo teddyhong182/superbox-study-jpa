5b133485ded1ef6a027fafba526e0e5ccd4c490a
Kiwon Hong <superbox@dtime.co>
2021-12-19 02:25:40 +0900

    고급 매핑 - Mapped SuperClass 매핑 정보 상속

---
+++ ex3-hello-jpa/src/main/java/com/superbox/study/ex3/BaseEntity.java
@@ -0,0 +1,46 @@
+package com.superbox.study.ex3;
+
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    private String createdBy;
+    private LocalDateTime createdAt;
+
+    private String lastModifiedBy;
+    private LocalDateTime lastModifiedAt;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public LocalDateTime getLastModifiedAt() {
+        return lastModifiedAt;
+    }
+
+    public void setLastModifiedAt(LocalDateTime lastModifiedAt) {
+        this.lastModifiedAt = lastModifiedAt;
+    }
+}
