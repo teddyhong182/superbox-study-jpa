9973e518c6604f2f9cc60ece913e04e2d490816f
Kiwon Hong <superbox@dtime.co>
2021-12-20 00:26:57 +0900

    값 타입 - 값 타입 공유 참조 보완 방법

--- ex5-hello-jpa/src/main/java/ex5/JpaMain.java
+++ ex5-hello-jpa/src/main/java/ex5/JpaMain.java
@@ -23,9 +23,12 @@ public class JpaMain {
             member.setHomeAddress(address);
             em.persist(member);
 
+            // 이렇게 처리해서 보완
+            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
             Member member2 = new Member();
             member2.setUsername("member2");
-            member2.setHomeAddress(address);
+            member2.setHomeAddress(copyAddress);
             em.persist(member2);
 
             member.getHomeAddress().setCity("newCity");
