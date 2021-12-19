6c985b9bde2a5ce9996f714e4f897bbd6c58b90d
Kiwon Hong <superbox@dtime.co>
2021-12-19 23:27:31 +0900

    영속성 전이와 고아 객체 - 영속성 전이 옵션

--- ex4-hello-jpa/src/main/java/com/superbox/study/ex4/JpaMain.java
+++ ex4-hello-jpa/src/main/java/com/superbox/study/ex4/JpaMain.java
@@ -17,32 +17,45 @@ public class JpaMain {
 
         try {
 
-            Team team1 = new Team();
-            team1.setName("teamA");
-            em.persist(team1);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team1);
-            em.persist(member1);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            em.flush();
-            em.clear();
+            // 이렇게 3번을 persist 를 호출 해야 함 이를 해결 위해 CASCADE OPTION 사용
+            em.persist(parent);
 
-            // 조인 후 쿼리 실행
-            Member m = em.find(Member.class, member1.getId());
+//            em.persist(child1);
+//            em.persist(child2);
 
-            // 프록시가 아닌 초기화 된 객체
-            System.out.println("m = " + m.getTeam().getClass());
 
-            // 이 시점에 쿼리 실행
-            m.getTeam().getName();
-
-            // JPQL 에서 N + 1 문제를 발생시킨다.
-            // EAGER 인 경우 조회된 MEMBER 의 TEAM 를 쿼리를 나눠서 조회한다. (조회해야할 TEAM 만큼)
-            List<Member> members = em.createQuery("select m from Member m", Member.class)
-                    .getResultList();
-
+//            Team team1 = new Team();
+//            team1.setName("teamA");
+//            em.persist(team1);
+//
+//            Member member1 = new Member();
+//            member1.setUsername("member1");
+//            member1.setTeam(team1);
+//            em.persist(member1);
+//
+//            em.flush();
+//            em.clear();
+//
+//            // 조인 후 쿼리 실행
+//            Member m = em.find(Member.class, member1.getId());
+//
+//            // 프록시가 아닌 초기화 된 객체
+//            System.out.println("m = " + m.getTeam().getClass());
+//
+//            // 이 시점에 쿼리 실행
+//            m.getTeam().getName();
+//
+//            // JPQL 에서 N + 1 문제를 발생시킨다.
+//            // EAGER 인 경우 조회된 MEMBER 의 TEAM 를 쿼리를 나눠서 조회한다. (조회해야할 TEAM 만큼)
+//            List<Member> members = em.createQuery("select m from Member m", Member.class)
+//                    .getResultList();
 
 
 //            Member member1 = new Member();
---
+++ ex4-hello-jpa/src/main/java/com/superbox/study/ex4/Parent.java
@@ -0,0 +1,41 @@
+package com.superbox.study.ex4;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Parent {
+    @Id
+    @GeneratedValue
+    @Column(name = "PARENT_ID")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
+    private List<Child> childList = new ArrayList<>();
+
+    public void addChild(Child child) {
+        childList.add(child);
+        child.setParent(this);
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
