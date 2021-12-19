f958132bae411ab7307022fa485a0b3f8ae4783c
Kiwon Hong <superbox@dtime.co>
2021-12-19 17:27:43 +0900

    프록시와 연관관계 관리 - 참고 객체 사용

--- ex4-hello-jpa/src/main/java/com/superbox/study/ex4/JpaMain.java
+++ ex4-hello-jpa/src/main/java/com/superbox/study/ex4/JpaMain.java
@@ -15,6 +15,23 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Member member = new Member();
+            member.setUsername("Hello");
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+//            Member findMember = em.find(Member.class, member.getId());
+
+            Member findMember = em.getReference(Member.class, member.getId());  // 엔티티를 사용하는 시점에서 쿼리가 실행
+
+            System.out.println("findMember.getClass() = " + findMember.getClass()); // 이것의 정체는?
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.username = " + findMember.getUsername());
+
             tx.commit();
         } catch (Exception e) {
             System.out.println(e.getMessage());
--- ex4-hello-jpa/src/main/java/com/superbox/study/ex4/Member.java
+++ ex4-hello-jpa/src/main/java/com/superbox/study/ex4/Member.java
@@ -12,4 +12,19 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
