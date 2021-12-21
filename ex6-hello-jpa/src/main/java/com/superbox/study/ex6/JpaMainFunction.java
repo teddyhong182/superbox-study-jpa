0c98d549a277c79cee006cded196decd4f6711a1
Kiwon Hong <superbox@dtime.co>
2021-12-22 02:11:37 +0900

    JPQL 문법 (함수)

---
+++ ex6-hello-jpa/src/main/java/com/superbox/study/ex6/JpaMainFunction.java
@@ -0,0 +1,56 @@
+package com.superbox.study.ex6;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMainFunction {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("team1");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.setTeam(team);
+            member.setType(MemberType.USER);
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+//            String qlString = "select concat('a', 'b') from Member m ";
+            String qlString = "select size(t.members) from Team t ";
+//            String qlString = "select function('group_', m.name) from Member m ";
+
+            List<Object[]> resultList = em.createQuery(qlString).getResultList();
+
+            for (Object[] objects : resultList) {
+                System.out.println("objects[0] = " + objects[0]);
+                System.out.println("objects[1] = " + objects[1]);
+                System.out.println("objects[2] = " + objects[2]);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            tx.rollback();
+
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
