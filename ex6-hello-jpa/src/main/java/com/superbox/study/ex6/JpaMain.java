f52c0cc7debaafe0046628791e946738f9522850
Kiwon Hong <superbox@dtime.co>
2021-12-22 01:03:20 +0900

    JPQL 문법 - 파라미터 처리

--- ex6-hello-jpa/src/main/java/com/superbox/study/ex6/JpaMain.java
+++ ex6-hello-jpa/src/main/java/com/superbox/study/ex6/JpaMain.java
@@ -18,19 +18,25 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
 
-            List<Member> resultList = query1.getResultList();
-
-            for (Member m : resultList) {
-
-            }
-
-            // 값이 정확히 하나 인 경우만 사용 가능 (없는 경우 exception, 다수 인 경우 exception)
-            Member singleResult = query1.getSingleResult();
-            // spring data jpa -> 없는 경우 정상 처리 가능 함
-            // 기본 사용시 try catch 필요
+            query.setParameter("username", "member1");
+            Member singleResult = query.getSingleResult();
             System.out.println("singleResult = " + singleResult);
+
+//            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+//
+//            List<Member> resultList = query1.getResultList();
+//
+//            for (Member m : resultList) {
+//
+//            }
+//
+//            // 값이 정확히 하나 인 경우만 사용 가능 (없는 경우 exception, 다수 인 경우 exception)
+//            Member singleResult = query1.getSingleResult();
+//            // spring data jpa -> 없는 경우 정상 처리 가능 함
+//            // 기본 사용시 try catch 필요
+//            System.out.println("singleResult = " + singleResult);
 
 //            TypedQuery<String> query2 = em.createQuery("select m.username from Member m", String.class);
 //            Query query3 = em.createQuery("select m.username, m.age from Member m");
