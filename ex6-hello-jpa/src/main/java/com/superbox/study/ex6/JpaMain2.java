c290e1177efbb35bf92cd166b352a9c0d3142a9b
Kiwon Hong <superbox@dtime.co>
2021-12-22 01:25:29 +0900

    JPQL 문법 (페이징) - 알아서 됨

---
+++ ex6-hello-jpa/src/main/java/com/superbox/study/ex6/JpaMain2.java
@@ -0,0 +1,44 @@
+package com.superbox.study.ex6;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain2 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            for (int i = 0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("member1");
+                member.setAge(10);
+                em.persist(member);
+            }
+
+            em.flush();
+            em.clear();
+
+            List<Member> resultList = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                    .setFirstResult(0).setMaxResults(10).getResultList();
+
+            System.out.println("resultList.size() = " + resultList.size());
+
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            tx.rollback();
+
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
