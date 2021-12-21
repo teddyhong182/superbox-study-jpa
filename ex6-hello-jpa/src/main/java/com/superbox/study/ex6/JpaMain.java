16b2ea2bdb975e8793adc7169359affa5f0697a4
Kiwon Hong <superbox@dtime.co>
2021-12-22 00:52:55 +0900

    JPQL 문법 - 프로젝트 셋팅

---
+++ ex6-hello-jpa/src/main/java/com/superbox/study/ex6/JpaMain.java
@@ -0,0 +1,33 @@
+package com.superbox.study.ex6;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setUsername("member1");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            tx.rollback();
+
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
